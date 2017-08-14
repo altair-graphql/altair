5bf3a99eaee958db9dfeb761a7e4b13e6b85d343
imolorhe <samuelimolo4real@gmail.com>
2017-08-14 21:48:01 +0100

    Updated electron main.js altair protocol register.

--- electron/main.js
+++ electron/main.js
@@ -10,55 +10,66 @@ let win
 
 function createWindow () {
 
-    // TODO - Use the altair protocol to replace the file protocol
-    protocol.registerBufferProtocol('altair', (request, callback) => {
-        const url = request.url.substr(7);
-        fs.readFile(path.normalize(`${url}`), 'utf8', function (err, data) {
-            if (err) {
-                return console.log('Error loading file to buffer.', err);
-            }
+  // TODO - Use the altair protocol to replace the file protocol
+  protocol.registerBufferProtocol('altair', (request, callback) => {
+    let url = request.url.substr(7);
 
-            // Load the data from the file into a buffer and pass it to the callback
-            // Using the mime package to get the mime type for the file, based on the file name
-            callback({ mimeType: mime.lookup(url), data: new Buffer(data) });
-            // console.log(data);
-        });
-        // callback({path: path.normalize(`${__dirname}/${url}`)})
-    }, (error) => {
-        if (error) console.error('Failed to register protocol')
+    // In windows, the url comes as altair://c/Users/Jackie/Documents/projects/altair/dist/index.html
+    // We also need to strip out the //c from the path
+    // TODO - Find a better way of handling this
+    if (process.platform === 'win32') {
+      url = request.url.substr(10);
+    }
+
+    // Maybe this could work?
+    // console.log('::>>', path.join(__dirname, '../dist', path.basename(request.url)));
+
+    fs.readFile(path.normalize(`${url}`), 'utf8', function (err, data) {
+      if (err) {
+        return console.log('Error loading file to buffer.', err);
+      }
+
+      // Load the data from the file into a buffer and pass it to the callback
+      // Using the mime package to get the mime type for the file, based on the file name
+      callback({ mimeType: mime.lookup(url), data: new Buffer(data) });
+      // console.log(data);
     });
+    // callback({path: path.normalize(`${__dirname}/${url}`)})
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  });
 
-    // Create the browser window.
-    win = new BrowserWindow({
-      width: 1280,
-      height: 800,
-      titleBarStyle: 'hidden-inset'
-    });
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    titleBarStyle: 'hidden-inset'
+  });
 
-    // Populate the application menu
-    createMenu();
+  // Populate the application menu
+  createMenu();
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, '../dist/index.html'),
-        protocol: 'altair:',
-        slashes: true
-    }));
-    // win.loadURL('altair://' + __dirname + '/../dist/index.html')
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '../dist/index.html'),
+    protocol: 'altair:',
+    slashes: true
+  }));
+  // win.loadURL('altair://' + __dirname + '/../dist/index.html')
 
-    // Open the DevTools.
-    // win.webContents.openDevTools()
+  // Open the DevTools.
+  // win.webContents.openDevTools()
 
-    // Prevent the app from navigating away from the app
-    win.webContents.on('will-navigate', (e, url) => e.preventDefault());
+  // Prevent the app from navigating away from the app
+  win.webContents.on('will-navigate', (e, url) => e.preventDefault());
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
-    })
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  })
 }
 
 function createMenu () {
@@ -136,24 +147,24 @@ protocol.registerStandardSchemes(['altair']);
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
-})
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
