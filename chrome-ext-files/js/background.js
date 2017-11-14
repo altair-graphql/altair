a6861b21f29f81572bfd11fc27e112563e8f930e
Samuel Imolorhe <samuel.imolorhe@booking.com>
2017-11-14 23:03:44 +0100

    Added functionality to ensure only one open extension tab. For #144.

--- chrome-ext-files/js/background.js
+++ chrome-ext-files/js/background.js
@@ -1,3 +1,38 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url: 'index.html'});
-});
+(function () {
+  var curTabId = null;
+
+  // Create a new tab for the extension
+  function createNewTab() {
+    chrome.tabs.create({ url: 'index.html' }, function (tab) {
+      curTabId = tab.id;
+    });
+  }
+
+  // Focus on the open extension tab
+  function focusTab(tabId) {
+    var updateProperties = { "active": true };
+    chrome.tabs.update(tabId, updateProperties, function (tab) { });
+  }
+
+  // Open the extension tab when the extension icon is clicked
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    if (!curTabId) {
+      createNewTab();
+    } else {
+      chrome.tabs.get(curTabId, function (tab) {
+        if (tab) {
+          focusTab(curTabId);
+        } else {
+          createNewTab();
+        }
+      });
+    }
+  });
+
+  // When a tab is closed, check if it is the extension tab that was closed, and unset curTabId
+  chrome.tabs.onRemoved.addListener(function (tabId) {
+    if (tabId === curTabId) {
+      curTabId = null;
+    }
+  });
+})();
