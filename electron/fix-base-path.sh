0ac1fcda992ace0ad6557b2b15e3fc925c03ff94
imolorhe <samuelimolo4real@gmail.com>
2017-11-20 00:50:30 +0100

    Updated fix-base-path script to hopefully fix linux electron build.

--- electron/fix-base-path.sh
+++ electron/fix-base-path.sh
@@ -2,4 +2,8 @@
 
 OLD_BASE='<base href="/">'
 NEW_BASE='<base href="./">'
-sed -i '' "s|$OLD_BASE|$NEW_BASE|g" ./dist/index.html
+sed "s|$OLD_BASE|$NEW_BASE|g" ./dist/index.html > ./dist/index.tmp
+
+# Replace the index.html with the generated file
+rm ./dist/index.html
+mv ./dist/index.tmp ./dist/index.html
