0041c5b73f510472f5f51c0ac8ef457eab4a35d7
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-05-13 17:20:51 +0200

    Fixed broken specs.

--- src/app/components/import-curl-dialog/import-curl-dialog.component.spec.ts
+++ src/app/components/import-curl-dialog/import-curl-dialog.component.spec.ts
@@ -2,13 +2,26 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImportCurlDialogComponent } from './import-curl-dialog.component';
 
+import { FormsModule } from '@angular/forms';
+import { ClarityModule } from 'clarity-angular';
+import { CodemirrorModule } from 'ng2-codemirror';
+import { TranslateModule } from '@ngx-translate/core';
+
+import * as services from '../../services';
+
 describe('ImportCurlDialogComponent', () => {
   let component: ImportCurlDialogComponent;
   let fixture: ComponentFixture<ImportCurlDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImportCurlDialogComponent ]
+      declarations: [ ImportCurlDialogComponent ],
+      imports: [
+        FormsModule,
+        CodemirrorModule,
+        ClarityModule.forRoot(),
+        TranslateModule.forRoot()
+      ],
     })
     .compileComponents();
   }));
