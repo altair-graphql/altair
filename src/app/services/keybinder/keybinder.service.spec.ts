968920febcbd2acfc0a09d46e43fff730257d00a
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-07-19 02:07:04 +0200

    Fixed broken tests.

--- src/app/services/keybinder/keybinder.service.spec.ts
+++ src/app/services/keybinder/keybinder.service.spec.ts
@@ -1,11 +1,27 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { KeybinderService } from './keybinder.service';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { WindowService } from '../window.service';
+import { DbService } from '../db.service';
 
 describe('KeybinderService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [KeybinderService]
+      providers: [
+        KeybinderService,
+        WindowService,
+        DbService,
+        {
+          provide: Store, useValue: {
+            subscribe: () => { },
+            select: () => [],
+            map: () => Observable.empty(),
+            dispatch: () => { }
+          }
+        }
+      ]
     });
   });
 
