2f674f86ef0e7e1e8c9ac28cbf4e43fadcb0d6f5
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-10-06 05:27:43 +0200

    Implemented link handler. Added collection feedback component.

--- src/app/utils/events.ts
+++ src/app/utils/events.ts
@@ -9,7 +9,28 @@
 export const on = (eventName, elSelector, fn) => {
   document.body.addEventListener(eventName, function(e) {
     if (e.target && e.target.matches(elSelector)) {
-      fn(e);
+      fn.apply(e.target, [e]);
+    }
+  });
+};
+
+export const handleExternalLinks = () => {
+  on('click', 'a', function(e) {
+    const url = (this.href || '');
+
+    if (!url.replace(/#.*$/, '')) {
+      // Handle hashes
+      return;
+    }
+    e.preventDefault();
+
+    // If electron app
+    if (window['process'] && window['process'].versions['electron']) {
+      const electron = window['require']('electron');
+      electron.shell.openExternal(url);
+    } else {
+      const win = window.open(url, '_blank');
+      win.focus();
     }
   });
 };
