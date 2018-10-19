fbb639f3a202e916d8177884fc90753eb5d7743d
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-10-19 21:52:04 +0200

    Updated altair packages. Added options for customizing altair npm packages.

--- packages/altair-static/scripts/prepare_dist.js
+++ packages/altair-static/scripts/prepare_dist.js
@@ -4,43 +4,53 @@ const fs = require('fs');
 
 ncp.limit = 16;
 
-const distSrc = path.join(__dirname, '../../../dist');
-const distDestination = path.join(__dirname, '../dist');
+const distSrc = path.join(__dirname, '../../../dist'); // From the main altair dist folder
+const distDestination = path.join(__dirname, '../build/dist'); // To altair-static dist folder
+if (!fs.existsSync(distDestination)){
+  fs.mkdirSync(distDestination);
+}
+
 const srcDir = path.resolve(__dirname, '../src');
 const buildDir = path.resolve(__dirname, '../build');
 
 const indexHtmlFile = path.join(distDestination, 'index.html');
 
-const cdnFile = fileName => `https://cdn.jsdelivr.net/npm/altair-static/dist/${fileName}`;
+// const cdnFile = fileName => `https://cdn.jsdelivr.net/npm/altair-static/dist/${fileName}`;
 
+/**
+ * Copy dist files into dist directory.
+ * Set base to ./
+ * Set the scripts and styles in template.html
+ * Add template.html to dist directory.
+ */
 ncp(distSrc, distDestination, function (err) {
   if (err) {
     return console.error(err);
   }
   console.log('Done copying dist folder!');
 
-  let indexHtmlStr = fs.readFileSync(indexHtmlFile, 'utf8');
+  // let indexHtmlStr = fs.readFileSync(indexHtmlFile, 'utf8');
 
   // Adjust the base URL to be relative to the current path
-  indexHtmlStr = indexHtmlStr.replace('<base href="/">', '<base href="https://cdn.jsdelivr.net/npm/altair-static/dist/">'
-    /*'<base href="./">'*/);
+  // indexHtmlStr = indexHtmlStr.replace('<base href="/">', '<base href="https://cdn.jsdelivr.net/npm/altair-static/dist/">'
+    // /*'<base href="./">'*/);
 
   // Write the new string back to file
-  fs.writeFileSync(indexHtmlFile, indexHtmlStr, 'utf8');
+  // fs.writeFileSync(indexHtmlFile, indexHtmlStr, 'utf8');
 
   try {
     const stats = JSON.parse(fs.readFileSync(path.resolve(distSrc, 'stats.json')));
 
-    let buildIndexHtmlStr = fs.readFileSync(path.resolve(srcDir, 'index.html'), 'utf8');
-    buildIndexHtmlStr = buildIndexHtmlStr.replace('<base href="/">', '<base href="https://cdn.jsdelivr.net/npm/altair-static/dist/">'
-      /*'<base href="./">'*/
-    );
-    buildIndexHtmlStr = buildIndexHtmlStr.replace('[% STYLES_FILE %]', stats.assetsByChunkName.styles);
-    buildIndexHtmlStr = buildIndexHtmlStr.replace('[% INLINE_SCRIPT %]', stats.assetsByChunkName.inline);
-    buildIndexHtmlStr = buildIndexHtmlStr.replace('[% POLYFILLS_SCRIPT %]', stats.assetsByChunkName.polyfills);
-    buildIndexHtmlStr = buildIndexHtmlStr.replace('[% MAIN_SCRIPT %]', stats.assetsByChunkName.main);
+    let htmlString = fs.readFileSync(path.resolve(srcDir, 'index.html'), 'utf8')
+      // Set base to ./
+      .replace('<base href="/">', '<base href="./">')
+      // Set scripts and styles
+      .replace('[% STYLES_FILE %]', stats.assetsByChunkName.styles)
+      .replace('[% INLINE_SCRIPT %]', stats.assetsByChunkName.inline)
+      .replace('[% POLYFILLS_SCRIPT %]', stats.assetsByChunkName.polyfills)
+      .replace('[% MAIN_SCRIPT %]', stats.assetsByChunkName.main);
 
-    fs.writeFileSync(path.join(buildDir, 'index.html'), buildIndexHtmlStr);
+    fs.writeFileSync(path.join(distDestination, 'index.html'), htmlString);
 
   } catch(err) {
     console.error('stats.json not found', err);
--- packages/altair-static/src/index.ts
+++ packages/altair-static/src/index.ts
@@ -1,8 +1,31 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
-export const renderAltair = (opts) => {
-    const altairHtml = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
-    // console.log(altairHtml);
-    return altairHtml;
+export interface RenderOptions {
+    endpointURL?: string;
+    subscriptionsEndpoint?: string;
+    initialQuery?: string;
+}
+
+export const renderAltair = ({ endpointURL, subscriptionsEndpoint, initialQuery }: RenderOptions = {}) => {
+    const altairHtml = readFileSync(resolve(__dirname, 'dist/index.html'), 'utf8');
+
+    let renderedOptions = '';
+
+    if (endpointURL) {
+        renderedOptions += `window.__ALTAIR_ENDPOINT_URL__ = '${endpointURL}';`;
+    }
+    if (subscriptionsEndpoint) {
+        renderedOptions += `window.__ALTAIR_SUBSCRIPTIONS_ENDPOINT__ = '${subscriptionsEndpoint}';`;
+    }
+    if (initialQuery) {
+        renderedOptions += `window.__ALTAIR_INITIAL_QUERY__ = '${initialQuery}';`;
+    }
+
+    const renderedOptionsInScript = `<script>${renderedOptions}</script>`;
+    return altairHtml.replace('</body>', `${renderedOptionsInScript}</body>`);
 };
+
+export const getDistDirectory = () => resolve(__dirname, 'dist');
+
+export default renderAltair;
