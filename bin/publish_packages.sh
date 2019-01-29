cefd9941f243f896ca2c7080f5aa2a438c86f127
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-01-29 02:34:40 +0100

    Changed from-package to from-git.

--- bin/publish_packages.sh
+++ bin/publish_packages.sh
@@ -2,4 +2,4 @@
 
 git checkout staging
 lerna bootstrap
-lerna publish from-package --force-publish=* --no-git-tag-version --no-push --yes
+lerna publish from-git --force-publish=* --no-git-tag-version --no-push --yes
