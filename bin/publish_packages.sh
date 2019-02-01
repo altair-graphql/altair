dff74e76ac1dade10a62332cdbc517ec7ef9c55f
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-02-01 20:36:31 +0100

    Updated lerna publish packages script.

--- bin/publish_packages.sh
+++ bin/publish_packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git checkout staging
+# Get new tags from remote
+git fetch --tags
+
+# Get latest tag name
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+# Checkout latest tag
+git checkout $LATEST_TAG
 lerna bootstrap
 lerna publish from-git --force-publish=* --no-git-tag-version --no-push --yes
