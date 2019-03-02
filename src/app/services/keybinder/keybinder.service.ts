cbd14b4792bf957b2f8534cfe864be588a8dc3de
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-03-02 15:08:04 +0100

    Added environment variable usage tip. Added description to keyboard shortcuts. Closes #599.

--- src/app/services/keybinder/keybinder.service.ts
+++ src/app/services/keybinder/keybinder.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { bind, bindGlobal } from 'mousetrap';
+import * as mousetrap from 'mousetrap';
 import 'mousetrap-global-bind';
 import { Store } from '@ngrx/store';
 import { WindowService } from '../window.service';
@@ -8,12 +8,15 @@ import * as fromRoot from '../../reducers';
 
 import * as dialogsActions from '../../actions/dialogs/dialogs';
 import * as queryActions from '../../actions/query/query';
+import * as docsActions from '../../actions/docs/docs';
 
 @Injectable()
 export class KeybinderService {
 
   windowIds;
   activeWindowId = '';
+
+  private shortcuts = [];
 
   constructor(
     private store: Store<fromRoot.State>,
@@ -27,17 +30,48 @@ export class KeybinderService {
   }
 
   connect() {
-    bindGlobal('ctrl+shift+v', () =>
-      this.zone.run(() => this.store.dispatch(new dialogsActions.ToggleVariableDialogAction(this.activeWindowId))));
+    this.bindShortcut(
+      'ctrl+shift+v',
+      () => this.store.dispatch(new dialogsActions.ToggleVariableDialogAction(this.activeWindowId)),
+      'Toggle Variable Pane'
+    );
 
-    bindGlobal('ctrl+shift+h', () =>
-      this.zone.run(() => this.store.dispatch(new dialogsActions.ToggleHeaderDialogAction(this.activeWindowId))));
+    this.bindShortcut(
+      'ctrl+shift+h',
+      () => this.store.dispatch(new dialogsActions.ToggleHeaderDialogAction(this.activeWindowId)),
+      'Toggle Header Pane'
+    );
 
-    bindGlobal('ctrl+shift+r', () =>
-      this.zone.run(() => this.store.dispatch(new queryActions.SendIntrospectionQueryRequestAction(this.activeWindowId))));
+    this.bindShortcut(
+      'ctrl+shift+r',
+      () => this.store.dispatch(new queryActions.SendIntrospectionQueryRequestAction(this.activeWindowId)),
+      'Reload Docs'
+    );
 
-    bindGlobal(['command+enter', 'ctrl+enter'], () =>
-      this.zone.run(() => this.store.dispatch(new queryActions.SendQueryRequestAction(this.activeWindowId))));
+    this.bindShortcut(
+      'ctrl+shift+d',
+      () => this.store.dispatch(new docsActions.ToggleDocsViewAction(this.activeWindowId)),
+      'Toggle Docs'
+    );
+
+    this.bindShortcut(
+      ['command+enter', 'ctrl+enter'],
+      () => this.store.dispatch(new queryActions.SendQueryRequestAction(this.activeWindowId)),
+      'Send Request'
+    );
+  }
+
+  bindShortcut(keys, callback, description?: string) {
+    this.shortcuts.push({
+      keys,
+      description
+    });
+
+    return mousetrap.bindGlobal(keys, () => this.zone.run(callback));
+  }
+
+  getShortcuts() {
+    return this.shortcuts;
   }
 
 }
