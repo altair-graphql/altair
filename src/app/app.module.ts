29444bd1872a351cad769c48dee42034bfb9cfc1
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-04-27 12:16:08 +0200

    Added CUSTOM_ELEMENTS_SCHEMA schema to app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
@@ -102,6 +102,9 @@ const providers = [
     }),
   ],
   providers: providers,
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
