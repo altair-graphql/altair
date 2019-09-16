a8fa07057af2411cade9777c74f57654f802a335
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-09-16 09:15:03 +0200

    Added notification in global error handler.

--- src/app/error-handler.ts
+++ src/app/error-handler.ts
@@ -1,6 +1,8 @@
 import { ErrorHandler, Injector, ApplicationRef, Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { debug } from './utils/logger';
+import { NotifyService } from './services';
+import newGithubIssueUrl from 'new-github-issue-url';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -12,7 +14,24 @@ export class GlobalErrorHandler implements ErrorHandler {
       debug.error('Backend returned status code: ', error.status);
       debug.error('Response body:', error.message);
     } else {
-      debug.error('Application error:', error.message);
+      const notifyService = this.injector.get(NotifyService);
+      const errorMessage = error.message ? error.message : error.toString();
+      const issueTitle = 'Bug: Application error';
+      const issueBody = `Error message: ${errorMessage}\n\nError stack:\n\n\`\`\`\n${error.stack}\n\`\`\``;
+      const issueUrl = newGithubIssueUrl({
+        user: 'imolorhe',
+        repo: 'altair',
+        title: issueTitle,
+        body: issueBody,
+      });
+      debug.error('Application error:', errorMessage);
+      notifyService.error(`An error occured: ${errorMessage}`);
+      notifyService.warning(`If you think this is a bug, click here to report the bug.`, 'Altair', {
+        disableTimeOut: true,
+        data: {
+          url: issueUrl,
+        }
+      });
     }
     // appRef.tick();
   }
