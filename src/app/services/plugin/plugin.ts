e9a631175055d853a9144ba27807533181ceeacd
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-12-08 00:51:58 +0100

    Added plugin capabilities.

--- src/app/services/plugin/plugin.ts
+++ src/app/services/plugin/plugin.ts
@@ -1,17 +1,45 @@
 
+/**
+ * Defines the repository of the plugin.
+ * Used to know where to get the plugin from.
+ */
 export enum PluginSource {
   NPM = 'npm',
   GITHUB = 'github',
   URL = 'url',
 }
+
+/**
+ * Specifies the type of the plugin.
+ * Determines how the plugin would interact with Altair.
+ */
 export enum PluginType {
   SIDEBAR = 'sidebar',
 }
+
+/**
+ * Specifies the capabilities (functionalities) available to the plugin.
+ * In the future, this would be used to request the necessary permissions from the user.
+ */
+export enum PluginCapabilities {
+  'query:read',
+  'query:write',
+  'header:read',
+  'header:write',
+  'environment:read',
+  'environment:write',
+}
+
 export interface PluginSidebarOptions {
   element_name: string;
   icon: string;
 }
+
+/**
+ * Plugin Manifest Structure
+ */
 export interface PluginManifest {
+  // Version of manifest. Should be 1 for now.
   manifest_version: number;
   name: string;
   display_name: string;
@@ -22,12 +50,16 @@ export interface PluginManifest {
   type: PluginType;
   sidebar_opts?: PluginSidebarOptions;
   scripts: string[];
-  styles: string[];
+  styles?: string[];
+  // Plugin capabilities
+  capabilities?: PluginCapabilities[];
 }
+
 export interface PluginInstance {
   name: string;
   display_name: string;
   type: PluginType;
+  capabilities: PluginCapabilities[];
   sidebar_opts?: PluginSidebarOptions;
   props?: any;
   context?: any;
@@ -59,14 +91,16 @@ export interface PluginComponentData extends PluginInstance {
 
 export class AltairPlugin implements PluginInstance {
   type = PluginType.SIDEBAR;
-  sidebar_opts: PluginSidebarOptions | undefined;
+  sidebar_opts?: PluginSidebarOptions;
   isActive = false;
   props;
   context;
   display_name = '';
+  capabilities: PluginCapabilities[] = [ PluginCapabilities['query:read'], PluginCapabilities['query:write'] ];
   constructor(public name: string, public manifest: PluginManifest) {
     this.sidebar_opts = manifest.sidebar_opts;
     this.type = manifest.type;
     this.display_name = manifest.display_name || name;
+    this.capabilities = Array.from(new Set([ ...(manifest.capabilities || []), ...this.capabilities ]));
   }
 }
