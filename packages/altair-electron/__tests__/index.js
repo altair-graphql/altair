bb1a04aacc998db0249cce59ce47e6e0a352d59c
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-01-29 22:42:27 +0100

    Debugging electron test.

--- packages/altair-electron/__tests__/index.js
+++ packages/altair-electron/__tests__/index.js
@@ -8,12 +8,15 @@ chai.use(chaiAsPromised);
 
 let electronPath = path.join(__dirname, '../node_modules', '.bin', 'electron');
 const appPath = path.join(__dirname, '../');
+
+console.log(`Electron app path: ${appPath}`);
+
 if (process.platform === 'win32') {
   electronPath += '.cmd';
 }
 const app = new Application({
   path: electronPath,
-  args: [appPath],
+  args: [ appPath ],
   env: {
     ELECTRON_ENABLE_LOGGING: true,
     ELECTRON_ENABLE_STACK_DUMPING: true,
@@ -23,8 +26,13 @@ const app = new Application({
   requireName: 'electronRequire',
 
   // Uncomment this line to debug
-  chromeDriverArgs: [ 'remote-debugging-port=' + Math.floor(Math.random() * (9999 - 9000) + 9000) ]
+  // chromeDriverArgs: [ 'remote-debugging-port=' + Math.floor(Math.random() * (9999 - 9000) + 9000) ]
 });
+
+app.start().then(() => {
+  console.log('Started successfully.');
+}).catch(err => console.error(err))
+.finally(() => app.isRunning() && app.stop());
 
 const selectors = {
   windowSwitcherSelector: '.window-switcher:not(.window-switcher--new-window)',
