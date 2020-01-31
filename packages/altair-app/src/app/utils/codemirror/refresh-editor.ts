2305248ee7bd2c3fa512db768981f2934443cd75
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-02-01 00:15:16 +0100

    Removed setTimeout from refresh-editor

--- packages/altair-app/src/app/utils/codemirror/refresh-editor.ts
+++ packages/altair-app/src/app/utils/codemirror/refresh-editor.ts
@@ -1,11 +1,9 @@
 export const handleEditorRefresh = (cm: any) => {
   if (cm) {
-    setTimeout(() => {
-      if (cm && cm.display.wrapper.offsetHeight) {
-        if (cm.display.lastWrapHeight !== cm.display.wrapper.clientHeight) {
-          cm.refresh();
-        }
-      }
-    }, 50);
+    if (cm && cm.display.wrapper.offsetHeight) {
+      // if (cm.display.lastWrapHeight !== cm.display.wrapper.clientHeight) {
+        cm.refresh();
+      // }
+    }
   }
 };
