a5681ea5068f00689f05675b15a6bb508ce5004d
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-02-20 00:18:53 +0100

    Updated cwex regex in update_version script.

--- bin/update_version.sh
+++ bin/update_version.sh
@@ -10,8 +10,8 @@ NEW_VERSION=$1
 OLD_BASE='"version": ".*"'
 NEW_BASE="\"version\": \"$NEW_VERSION\""
 
-CWEX_OLD_BASE='version: .*'
-CWEX_NEW_BASE="version: $NEW_VERSION"
+CWEX_OLD_BASE=' version: .*'
+CWEX_NEW_BASE=" version: $NEW_VERSION"
 
 # Update version in manifest.json
 sed -i '' "s|$OLD_BASE|$NEW_BASE|g" ./chrome-ext-files/manifest.json
