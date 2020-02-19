06e4d36d8d23f9f356870cb796807b158e46c242
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-02-19 22:10:06 +0100

    Updated to v2.4.4

--- bin/update_version.sh
+++ bin/update_version.sh
@@ -10,11 +10,17 @@ NEW_VERSION=$1
 OLD_BASE='"version": ".*"'
 NEW_BASE="\"version\": \"$NEW_VERSION\""
 
+CWEX_OLD_BASE='version: .*'
+CWEX_NEW_BASE="version: $NEW_VERSION"
+
 # Update version in manifest.json
 sed -i '' "s|$OLD_BASE|$NEW_BASE|g" ./chrome-ext-files/manifest.json
 
 # Update version in package.json
 sed -i '' "s|$OLD_BASE|$NEW_BASE|g" ./package.json
+
+# Update version in cwex.yml
+sed -i '' "s|$CWEX_OLD_BASE|$CWEX_NEW_BASE|g" ./cwex.yml
 
 if (lerna --version); then
   # Set the lerna packages versions as well
