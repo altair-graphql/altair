07d8eed76c141107369e684cda156a00be8e7b75
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-08-08 16:33:44 +0200

    Check for update config before attempting update. Closes #1084

--- packages/altair-electron/src/updates.js
+++ packages/altair-electron/src/updates.js
@@ -55,34 +55,17 @@ autoUpdater.on('update-downloaded', () => {
   });
 });
 
+const canUpdate = () => {
+  // Don't check for updates if update config is not found (auto-update via electron is not supported)
+  return autoUpdater.app
+    && autoUpdater.app.appUpdateConfigPath
+    && !fs.existsSync(autoUpdater.app.appUpdateConfigPath);
+};
+
 const setupAutoUpdates = () => {
-  // autoUpdater.setFeedURL(feed);
-
-  // setInterval(() => {
-  //   autoUpdater.checkForUpdates();
-  // }, CHECK_UPDATE_INTERVAL);
-
-
-  // autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  //   const dialogOpts = {
-  //     type: 'info',
-  //     buttons: ['Restart', 'Later'],
-  //     title: 'Application Update',
-  //     message: process.platform === 'win32' ? releaseNotes : releaseName,
-  //     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  //   };
-
-  //   dialog.showMessageBox(dialogOpts, (response) => {
-  //     if (response === 0) {
-  //       autoUpdater.quitAndInstall();
-  //     }
-  //   });
-  // });
-
-  // autoUpdater.on('error', message => {
-  //   console.error('There was a problem updating the application');
-  //   console.error(message);
-  // });
+  if (!canUpdate()) {
+    return;
+  }
 
   log.transports.file.level = 'info';
   autoUpdater.logger = log;
@@ -107,12 +90,7 @@ const setupAutoUpdates = () => {
 // });
 
 const checkForUpdates = (menuItem) => {
-  if (
-    autoUpdater.app
-    && autoUpdater.app.appUpdateConfigPath
-    && !fs.existsSync(autoUpdater.app.appUpdateConfigPath)
-  ) {
-    // Don't check for updates if update config is not found (auto-update via electron is not supported)
+  if (!canUpdate()) {
     return;
   }
 
