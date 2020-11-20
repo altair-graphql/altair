03ff2aa330a5a9bb5ef6a55e973cae69019ce7d0
Andrew H Yi <andrew.yi50@gmail.com>
2020-11-20 14:35:41 -0500

    hack: make altair work with rails actioncable subscriptions

--- packages/altair-app/src/app/services/subscriptions/providers/ws.ts
+++ packages/altair-app/src/app/services/subscriptions/providers/ws.ts
@@ -1,41 +1,50 @@
 import { SubscriptionProvider, SubscriptionProviderExecuteOptions } from '../subscription-provider';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import ActionCable from 'actioncable'
+import ActionCableLink from 'graphql-ruby-client/dist/subscriptions/ActionCableLink'
 
 export class WebsocketSubscriptionProvider extends SubscriptionProvider {
-  client?: SubscriptionClient;
-
-  createClient() {
-    this.client = new SubscriptionClient(this.subscriptionUrl, {
-      reconnect: true,
-      connectionParams: this.connectionParams,
-      connectionCallback: this.extraOptions?.onConnected
-    });
-  }
+  subscription?: any;
 
   execute(options: SubscriptionProviderExecuteOptions) {
-    this.createClient();
-
-    if (!this.client) {
-      throw new Error('Could not create subscription client!');
-    }
+    const cable = ActionCable.createConsumer(this.subscriptionUrl) // must contain token param for auth reasons
+    const cableLink = new ActionCableLink({cable})
 
     return new Observable((subscriber) => {
-      return this.client!.request({
-        query: options.query,
-        variables: options.variables,
-        operationName: options.operationName,
-      }).subscribe({
-        next: (...args) =>  subscriber.next(...args),
-        error: (...args) => subscriber.error(...args),
-        complete: (...args) => subscriber.complete(...args),
+
+      var channelId = Math.round(Date.now() + Math.random() * 100000).toString(16)
+      var actionName = "execute"
+      this.subscription = cable.subscriptions.create(Object.assign({},{
+        channel: "GraphqlChannel",
+        channelId: channelId
+      }, {}), {
+        connected: function() {
+          console.log("~~~~~ CONNECTED")
+          this.perform(
+            actionName,
+            {
+              query: options.query,
+              variables: options.variables,
+              operationName: options.operationName
+            }
+          )
+        },
+        received: function(payload) {
+          console.log("~~~~~ RECEIVED")
+          if (payload.result.data || payload.result.errors) {
+            subscriber.next(payload.result)
+          }
+
+          if (!payload.more) {
+            subscriber.complete()
+          }
+        }
       })
     });
   }
 
   close() {
-    this.client?.unsubscribeAll();
-    this.client?.close();
-    this.client = undefined;
+    console.log("~~~~ CLOSE PLZ")
+    this.subscription.unsubscribe()
   }
 }
