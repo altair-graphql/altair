e3d9b4529c3475b9c6eaafe6b7b3e8f03f0b8e82
Samuel Imolorhe <samuelimolo4real@gmail.com>
2021-03-17 05:54:49 +0100

    Added new settings for proxy configuration. Added Jetbrains mono as default query font. Closes #1473. Closes #1434.

---
+++ packages/altair-electron/src/settings/constants.js
@@ -0,0 +1,11 @@
+const settingsStoreFileName = 'desktop_settings';
+
+const SETTINGS_STORE_EVENTS = {
+  GET_SETTINGS_DATA: 'GET_SETTINGS_DATA',
+  UPDATE_SETTINGS_DATA: 'UPDATE_SETTINGS_DATA',
+};
+
+module.exports = {
+  settingsStoreFileName,
+  SETTINGS_STORE_EVENTS,
+};
