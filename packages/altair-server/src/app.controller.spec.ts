ed55121af947565f18d16326aceab8713c2e161d
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-01-30 11:51:13 +0100

    fixed test

--- packages/altair-server/src/app.controller.spec.ts
+++ packages/altair-server/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello!"', () => {
+      expect(appController.getHello()).toBe('Hello!');
     });
   });
 });
--- packages/altair-server/src/app.service.ts
+++ packages/altair-server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worlds!';
+    return 'Hello!';
   }
 }
