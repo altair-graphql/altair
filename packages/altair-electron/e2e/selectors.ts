0f86171838de5b616b9e7f86407e6470f3369225
Samuel <samuelimolo4real@gmail.com>
2022-05-15 20:29:12 +0200

    Re-enable electron e2e tests using playwright. (#1769)
    * Re-enable electron e2e tests using playwright.
    For #1676

---
+++ packages/altair-electron/e2e/selectors.ts
@@ -0,0 +1,6 @@
+export const WINDOW_SWITCHER = '.window-switcher__item';
+export const WINDOW_SWITCHER_CLOSE = '.window-switcher__close';
+export const VISIBLE_WINDOW = 'app-window:not(.hide)';
+export const VISIBLE_WINDOW_URL_INPUT = `${VISIBLE_WINDOW} .url-box__input input`;
+export const VISIBLE_WINDOW_QUERY_CODEMIRROR = `${VISIBLE_WINDOW} .query-editor__input .CodeMirror`;
+export const VISIBLE_WINDOW_QUERY_EDITOR = `${VISIBLE_WINDOW_QUERY_CODEMIRROR} .CodeMirror-code`;
