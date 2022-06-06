f1fa70f25a8911dda9dbc0c974926e5c39817a85
Adam O <62804095+adam-osu@users.noreply.github.com>
2022-06-06 18:32:07 -0400

    Add tests to subscription-url-dialog.component (#1922)
    Change test for handleEditorRefresh call on change detection check

--- packages/altair-app/src/app/modules/altair/components/subscription-url-dialog/subscription-url-dialog.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/subscription-url-dialog/subscription-url-dialog.component.spec.ts
@@ -9,6 +9,7 @@ import * as services from '../../services';
 import { SubscriptionUrlDialogComponent } from './subscription-url-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../../modules/shared/shared.module';
+import * as refreshEditor from '../../utils/codemirror/refresh-editor';
 
 describe('SubscriptionUrlDialogComponent', () => {
   let component: SubscriptionUrlDialogComponent;
@@ -34,7 +35,32 @@ describe('SubscriptionUrlDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should call handleEditorRefresh after first change detection check', () => {
+    spyOn(refreshEditor, 'handleEditorRefresh');
+    fixture.detectChanges();
+
+    expect(refreshEditor.handleEditorRefresh).toHaveBeenCalled();
+  });
+
+  it("should handle subscriptionUrlChange", () => {
+    const subscriptionUrl = "test";
+    component.subscriptionUrl = subscriptionUrl;
+    component.subscriptionUrlChange.emit(subscriptionUrl);
+    expect(component.subscriptionUrl).toBe(subscriptionUrl);
+  });
+
+  it("should handle updateSubscriptionProviderId", () => {
+    spyOn(component.subscriptionProviderIdChange, "emit");
+
+    const providerId = "test";
+    component.updateSubscriptionProviderId(providerId);
+
+    expect(component.subscriptionProviderIdChange.emit).toHaveBeenCalledWith(
+      providerId
+    );
+  });
+
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
