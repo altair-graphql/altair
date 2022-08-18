9ba392fb0b9c5e4468e30db07ee6478be542f8af
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-08-18 10:23:23 +0200

    fixed ts error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
