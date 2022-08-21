28eb30ef15c1b5f2fab1d00225b63015ddaffd5c
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-08-21 17:02:16 +0200

    fixed electron test

--- packages/altair-electron/jest.config.js
+++ packages/altair-electron/jest.config.js
@@ -91,7 +91,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
