663888e05df767175e64b87bcc57ab2e27f7275e
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-08-28 16:47:50 +0200

    fixed test

--- packages/altair-app/jest.config.js
+++ packages/altair-app/jest.config.js
@@ -20,6 +20,6 @@ module.exports = {
     'src',
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!@angular|@firebase|@ngrx|lodash-es|altair-graphql-core|dexie|uuid|ngx-cookie-service|ngx-markdown)',
+    'node_modules/(?!@angular|@firebase|@ngrx|lodash-es|altair-graphql-core|angular-resizable-element|dexie|uuid|ngx-cookie-service|ngx-markdown)',
   ],
 };
