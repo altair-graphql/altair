9349c3aee43257e1c19c867473254b2807f113b3
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-10-08 13:00:01 +0200

    fixed tests

--- packages/altair-app/src/app/modules/altair/components/query-editor/query-editor.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/query-editor/query-editor.component.spec.ts
@@ -1,15 +1,22 @@
 import { expect, describe, it } from '@jest/globals';
 import { QueryEditorComponent } from './query-editor.component';
 import { SharedModule } from '../../modules/shared/shared.module';
-import { NgxTestWrapper, mount, mock } from '../../../../../testing';
+import {
+  NgxTestWrapper,
+  mount,
+  mock,
+  mockStoreFactory,
+} from '../../../../../testing';
 import { MockModule } from 'ng-mocks';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { GqlService, NotifyService } from '../../services';
+import { Store } from '@ngrx/store';
 
 describe('QueryEditorComponent', () => {
   let wrapper: NgxTestWrapper<QueryEditorComponent>;
 
   beforeEach(async () => {
+    const mockStore = mockStoreFactory();
     wrapper = await mount({
       component: QueryEditorComponent,
       imports: [MockModule(SharedModule)],
@@ -21,6 +28,10 @@ describe('QueryEditorComponent', () => {
         {
           provide: NotifyService,
           useValue: mock(),
+        },
+        {
+          provide: Store,
+          useValue: mockStore,
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
