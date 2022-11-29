1a4063421897b80d30617a5cbb265e7d33324b7a
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-11-29 23:08:56 +0100

    fixed test

--- packages/altair-app/src/app/modules/altair/components/loader/loader.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/loader/loader.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+import { MockModule } from 'ng-mocks';
 
 import { LoaderComponent } from './loader.component';
 
@@ -8,9 +11,9 @@ describe('LoaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoaderComponent ]
-    })
-    .compileComponents();
+      declarations: [LoaderComponent],
+      imports: [MockModule(TranslateModule), NoopAnimationsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoaderComponent);
     component = fixture.componentInstance;
