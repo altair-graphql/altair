1d001474898b218fdd7790844fce6fbc028911e1
Samuel <samuelimolo4real@gmail.com>
2022-11-29 23:45:50 +0100

    added loader component (#2056)
    * added loader component
    
    * fixed test

---
+++ packages/altair-app/src/app/modules/altair/components/loader/loader.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+import { MockModule } from 'ng-mocks';
+
+import { LoaderComponent } from './loader.component';
+
+describe('LoaderComponent', () => {
+  let component: LoaderComponent;
+  let fixture: ComponentFixture<LoaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoaderComponent],
+      imports: [MockModule(TranslateModule), NoopAnimationsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
