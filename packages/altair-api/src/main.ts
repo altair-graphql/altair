7827c44437b37780f3943bf040a021678c4fd7c0
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-02-14 10:09:56 +0100

    added newrelic above all modules

--- packages/altair-api/src/main.ts
+++ packages/altair-api/src/main.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('newrelic');
+
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
