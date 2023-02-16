9b01294fd44fb9f2cb6499235271127f99fa65b1
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-02-16 23:38:40 +0100

    fixed tests

--- packages/altair-api/src/query-collections/query-collections.controller.spec.ts
+++ packages/altair-api/src/query-collections/query-collections.controller.spec.ts
@@ -1,6 +1,7 @@
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from 'nestjs-prisma';
+import { TeamsService } from 'src/teams/teams.service';
 import { QueryCollectionsController } from './query-collections.controller';
 import { QueryCollectionsService } from './query-collections.service';
 
@@ -10,7 +11,12 @@ describe('QueryCollectionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QueryCollectionsController],
-      providers: [QueryCollectionsService, PrismaService, EventEmitter2],
+      providers: [
+        QueryCollectionsService,
+        PrismaService,
+        EventEmitter2,
+        TeamsService,
+      ],
     }).compile();
 
     controller = module.get<QueryCollectionsController>(
