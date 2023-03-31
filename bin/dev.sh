3bdeb44958dd8de5212061fdab3f40b32e830944
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-03-31 18:16:47 +0200

    added dev script to start all services

---
+++ bin/dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+ROOT=$(cd "$SCRIPT_DIR/../" && pwd)/
+cd "$ROOT"
+
+(cd packages/altair-api; docker compose up  --no-recreate -d)
+
+npx concurrently --kill-others "yarn start:app" "yarn start:api" "yarn start:redirect" "yarn start:dashboard" --names app,api,redirect,dashboard
