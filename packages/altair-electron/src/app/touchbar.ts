f2b3dee82eb792a1f2f363fa1e277268adcecd92
syedmasroor <syedmasroorulhasan2020@gmail.com>
2023-04-06 02:32:34 +0530

    HideDocs button is added

--- packages/altair-electron/src/app/touchbar.ts
+++ packages/altair-electron/src/app/touchbar.ts
@@ -4,6 +4,9 @@ import { ActionManager } from './actions';
 const { TouchBarButton, TouchBarSpacer } = TouchBar;
 
 export class TouchbarManager {
+  private showDocsButton: TouchBarButton;
+  private hideDocsButton: TouchBarButton;
+
   constructor(private actionManager: ActionManager) {}
 
   createTouchBar() {
@@ -18,11 +21,12 @@ export class TouchbarManager {
       click: () => this.actionManager.reloadDocs(),
     });
 
-    const showDocsButton = new TouchBarButton({
+    this.showDocsButton = new TouchBarButton({
       label: 'Show Docs',
       click: () => {
         this.actionManager.showDocs();
-        touchBar.escapeItem = spacer; // Set the escapeItem to spacer so the button disappears
+        this.removeShowDocsButton();
+        this.addHideDocsButton();
       },
     });
 
@@ -36,11 +40,39 @@ export class TouchbarManager {
         sendRequestButton,
         spacer,
         reloadDocsButton,
-        showDocsButton,
+        this.showDocsButton,
       ],
-      escapeItem: showDocsButton, // Set the initial escapeItem to showDocsButton
     });
 
     return touchBar;
+  }
+
+  private removeShowDocsButton() {
+    const { items } = this.showDocsButton.parent;
+    const index = items.indexOf(this.showDocsButton);
+    if (index !== -1) {
+      items.splice(index, 1);
+    }
+  }
+
+  private addHideDocsButton() {
+    this.hideDocsButton = new TouchBarButton({
+      label: 'Hide Docs',
+      click: () => {
+        this.actionManager.hideDocs();
+        this.removeHideDocsButton();
+        this.showDocsButton.addToTouchBar();
+      },
+    });
+    const { items } = this.showDocsButton.parent;
+    items.push(this.hideDocsButton);
+  }
+
+  private removeHideDocsButton() {
+    const { items } = this.hideDocsButton.parent;
+    const index = items.indexOf(this.hideDocsButton);
+    if (index !== -1) {
+      items.splice(index, 1);
+    }
   }
 }
