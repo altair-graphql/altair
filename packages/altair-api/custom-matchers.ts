a53d570b5e8d07ff97a646115cea70d080472c64
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2023-07-09 16:47:51 +0200

    Add unit tests for user service

---
+++ packages/altair-api/custom-matchers.ts
@@ -0,0 +1,136 @@
+function typeCheck(name: string, value: unknown, expectedType: string) {
+  const type = typeof value;
+  if (type != expectedType) {
+    return {
+      pass: false,
+      message: () =>
+        `expected ${name} (value: ${value}) to be ${expectedType}, but found ${type} instead.`,
+    };
+  }
+
+  return { pass: true, message: () => '' };
+}
+
+function toBeUser(user: any) {
+  let check = typeCheck('User.id', user?.id, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck('User.email', user?.email, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck('User.firstName', user?.firstName, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck('User.lastName', user?.lastName, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck('User.picture', user?.picture, 'string');
+  if (!check.pass) return check;
+
+  return {
+    pass: true,
+    message: () => `expected ${user} not to match the shape of a User object`,
+  };
+}
+
+function toBePlanConfig(planConfig: any) {
+  let check = typeCheck('PlanConfig.id', planConfig?.id, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'PlanConfig.stripeProductId',
+    planConfig?.stripeProductId,
+    'string'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'PlanConfig.maxQueryCount',
+    planConfig?.maxQueryCount,
+    'number'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'PlanConfig.maxTeamCount',
+    planConfig?.maxTeamCount,
+    'number'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'PlanConfig.maxTeamMemberCount',
+    planConfig?.maxTeamMemberCount,
+    'number'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'PlanConfig.allowMoreTeamMembers',
+    planConfig?.allowMoreTeamMembers,
+    'boolean'
+  );
+  if (!check.pass) return check;
+
+  return {
+    pass: true,
+    message: () =>
+      `expected ${planConfig} not to match the shape of a PlanConfig object`,
+  };
+}
+
+function toBeSubscriptionItem(subItem: any) {
+  let check = typeCheck('SubscriptionItem.id', subItem?.id, 'string');
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.object', subItem?.object, 'object');
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'SubscriptionItem.billing_thresholds',
+    subItem?.billing_thresholds,
+    'object'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.created', subItem?.created, 'number');
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.metadata', subItem?.metadata, 'object');
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.plan', subItem?.plan, 'object');
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.price', subItem?.price, 'object');
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'SubscriptionItem.subscription',
+    subItem?.subscription,
+    'string'
+  );
+  if (!check.pass) return check;
+
+  check = typeCheck('SubscriptionItem.tax_rates', subItem?.tax_rates, 'object');
+  if (!check.pass) return check;
+
+  check = typeCheck(
+    'SubscriptionItem.lastResponse',
+    subItem?.lastResponse,
+    'object'
+  );
+  if (!check.pass) return check;
+
+  return {
+    pass: true,
+    message: () =>
+      `expected ${subItem} not to match the shape of a SubscriptionItem object`,
+  };
+}
+
+expect.extend({
+  toBeUser,
+  toBePlanConfig,
+  toBeSubscriptionItem,
+});
--- packages/altair-api/jest.config.js
+++ packages/altair-api/jest.config.js
@@ -12,4 +12,5 @@ module.exports = {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['./custom-matchers.ts'],
 };
