c2f7b3f86ff33891f69bc3ddedccd1cb2fa82b20
Tiago404 <tiagosousasa@gmail.com>
2023-08-18 20:36:25 -0400

    Added unit test to account-dialog.component

--- packages/altair-app/src/app/modules/altair/components/account-dialog/account-dialog.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/account-dialog/account-dialog.component.spec.ts
@@ -1,5 +1,7 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { describe, expect, it } from '@jest/globals';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { mount, NgxTestWrapper } from '../../../../../testing';
 import { SharedModule } from '../../modules/shared/shared.module';
 
@@ -7,18 +9,31 @@ import { AccountDialogComponent } from './account-dialog.component';
 
 describe('AccountDialogComponent', () => {
   let wrapper: NgxTestWrapper<AccountDialogComponent>;
+  let component: AccountDialogComponent;
+  let fixture: ComponentFixture<AccountDialogComponent>;
 
   beforeEach(async () => {
     wrapper = await mount({
       component: AccountDialogComponent,
       declarations: [AccountDialogComponent],
       providers: [],
-      imports: [SharedModule],
+      imports: [SharedModule, TranslateModule.forRoot(),],
       schemas: [NO_ERRORS_SCHEMA],
     });
+    fixture = TestBed.createComponent(AccountDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(wrapper.component.nativeElement).toMatchSnapshot();
+  });
+
+  it('should handle submitLogin', () => {
+    jest.spyOn(component.handleLoginChange, 'emit');
+
+    component.submitLogin();
+
+    expect(component.handleLoginChange.emit).toHaveBeenCalled();
   });
 });
