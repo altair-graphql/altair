5d02e88702914fa870e2529dfa4496498f858e8a
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-09-24 22:54:21 +0200

    restore custom notarize script

---
+++ packages/altair-electron/scripts/notarize.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+const path = require('path');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log('Notarizing application');
+  await notarize({
+    tool: 'notarytool',
+    // appBundleId: 'com.xkoji.altair',
+    appPath: path.resolve(appOutDir, `${appName}.app`),
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  }).then(res => console.log('Notarization completed!') || res);
+};
