a73402f913f5e6da1beb6f78194b7d2d5779872d
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-09-29 02:18:44 +0200

    removed unsafe usage of shell.openExternal

--- packages/altair-app/src/app/modules/altair/components/fork-repo/fork-repo.component.ts
+++ packages/altair-app/src/app/modules/altair/components/fork-repo/fork-repo.component.ts
@@ -12,18 +12,9 @@ export class ForkRepoComponent {
   externalLink(e: Event, url: string) {
     e.preventDefault();
 
-    // If electron app
-    if (
-      (window as any).process &&
-      (window as any).process.versions['electron']
-    ) {
-      const electron = (window as any).require('electron');
-      electron.shell.openExternal(url);
-    } else {
-      const win = window.open(url, '_blank');
-      if (win) {
-        win.focus();
-      }
+    const win = window.open(url, '_blank');
+    if (win) {
+      win.focus();
     }
   }
 }
