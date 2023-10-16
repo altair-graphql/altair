dbd4f8420d53f391f7958004461cc5aaa5940eba
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-10-16 05:55:49 +0200

    updated uuid

--- bin/dev.sh
+++ bin/dev.sh
@@ -10,4 +10,5 @@ cd "$ROOT"
 
 yarn
 
+# stripe login (if API key is expired)
 npx concurrently --kill-others "yarn start:app" "yarn start:api:dev" "yarn start:redirect" "yarn start:dashboard" "yarn start:stripe:listen" --names app,api,redirect,dashboard,stripe
