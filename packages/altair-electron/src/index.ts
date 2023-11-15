26e8f4817735ec8a3c64c53620038a2fd0cef766
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-11-16 00:39:59 +0100

    use app.getVersion()

--- packages/altair-electron/src/index.ts
+++ packages/altair-electron/src/index.ts
@@ -1,11 +1,11 @@
 import unhandled from 'electron-unhandled';
 import * as Sentry from '@sentry/electron';
 import { ElectronApp } from './app';
-import { version } from '../package.json';
+import { app } from 'electron';
 
 Sentry.init({
   dsn: 'https://1b08762f991476e3115e1ab7d12e6682@o4506180788879360.ingest.sentry.io/4506198594813952',
-  release: version,
+  release: app.getVersion(),
 });
 new ElectronApp();
 unhandled({ showDialog: false });
