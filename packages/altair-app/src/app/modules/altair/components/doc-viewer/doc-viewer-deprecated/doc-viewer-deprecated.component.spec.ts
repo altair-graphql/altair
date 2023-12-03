325e0442aa5c38532b516af4c32f9fe2a996564b
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-12-03 15:20:12 +0100

    Added deprecated to enums

---
+++ packages/altair-app/src/app/modules/altair/components/doc-viewer/doc-viewer-deprecated/doc-viewer-deprecated.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocViewerDeprecatedComponent } from './doc-viewer-deprecated.component';
+
+describe('DocViewerDeprecatedComponent', () => {
+  let component: DocViewerDeprecatedComponent;
+  let fixture: ComponentFixture<DocViewerDeprecatedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DocViewerDeprecatedComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DocViewerDeprecatedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
