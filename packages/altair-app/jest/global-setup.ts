9f229f8e60387f3c706356e4132bfa07bdd032c1
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-12-20 01:39:01 +0100

    updated tests

--- packages/altair-app/jest/global-setup.ts
+++
@@ -1,9 +0,0 @@
-import { runNgccJestProcessor } from './ngcc-jest-processor';
-
-export default async () => {
-  const ngJestConfig = (globalThis as any).ngJest;
-  const tsconfig = ngJestConfig?.tsconfig;
-  if (!ngJestConfig?.skipNgcc) {
-    runNgccJestProcessor(tsconfig);
-  }
-};
