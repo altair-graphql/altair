2b16fce9c50aa35e0d7a037510e31efec73e1bfe
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-01-17 16:32:20 +0100

    testing changes

--- packages/altair-app/src/app/modules/altair/services/sharing/sharing.service.spec.ts
+++ packages/altair-app/src/app/modules/altair/services/sharing/sharing.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SharingService } from './sharing.service';
+import { mock } from '../../../../../testing';
+import * as services from './../../services';
 
 describe('SharingService', () => {
   let service: SharingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: services.ApiService, useValue: mock() },
+        { provide: services.AccountService, useValue: mock() },
+        { provide: services.WindowService, useValue: mock() },
+        { provide: services.NotifyService, useValue: mock() },
+      ],
+    });
     service = TestBed.inject(SharingService);
   });
 
