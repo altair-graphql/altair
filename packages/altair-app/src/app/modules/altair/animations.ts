eacd504986c29c441e514e53ea7a4ab50c393d27
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-03-30 21:29:12 +0100

    updated loader

--- packages/altair-app/src/app/modules/altair/animations.ts
+++ packages/altair-app/src/app/modules/altair/animations.ts
@@ -10,3 +10,16 @@ export const fadeInOutAnimationTrigger = trigger('fadeInOutAnimation', [
     animate('.3s ease', style({ opacity: 0 })),
   ]),
 ]);
+export const fadeGridGrowInOutAnimationTrigger = trigger(
+  'fadeGridGrowInOutAnimation',
+  [
+    transition(':enter', [
+      style({ opacity: 0, gridTemplateRows: '0fr' }),
+      animate('.3s ease', style({ opacity: 1, gridTemplateRows: '1fr' })),
+    ]),
+    transition(':leave', [
+      style({ opacity: 1, gridTemplateRows: '1fr'}),
+      animate('.3s ease', style({ opacity: 0, gridTemplateRows: '0fr'})),
+    ]),
+  ]
+);
