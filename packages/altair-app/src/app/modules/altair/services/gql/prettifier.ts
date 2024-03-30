64da2f148c736632209eaf00eda5bd0c97d337e7
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-03-30 20:02:52 +0100

    updated prettier

--- packages/altair-app/src/app/modules/altair/services/gql/prettifier.ts
+++ packages/altair-app/src/app/modules/altair/services/gql/prettifier.ts
@@ -1,10 +1,10 @@
 export const prettify = async (query: string, tabWidth = 2) => {
   const prettier = await import('prettier/standalone');
-  const prettierGraphql = await import('prettier/parser-graphql');
+  const prettierGraphql = await import('prettier/plugins/graphql');
   // return print(parse(query));
   return prettier.format(query, {
     parser: 'graphql',
-    plugins: [prettierGraphql],
+    plugins: [prettierGraphql.default],
     tabWidth,
   });
 };
