9046bcfb5701210e2538fe8a768d38c3894944d0
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-04-05 23:25:19 +0200

    truncated long query names in request button

--- packages/altair-app/src/app/modules/altair/pipes/index.ts
+++ packages/altair-app/src/app/modules/altair/pipes/index.ts
@@ -4,9 +4,16 @@ import { KeysPipe } from './keys/keys.pipe';
 import { SortPipe } from './sort/sort.pipe';
 import { CollectionTransformPipe } from './collection-transform/collection-transform.pipe';
 import { DefaultPipe } from './default/default.pipe';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
-  declarations: [KeysPipe, SortPipe, CollectionTransformPipe, DefaultPipe],
-  exports: [KeysPipe, SortPipe, CollectionTransformPipe, DefaultPipe],
+  declarations: [
+    KeysPipe,
+    SortPipe,
+    CollectionTransformPipe,
+    DefaultPipe,
+    TruncatePipe,
+  ],
+  exports: [KeysPipe, SortPipe, CollectionTransformPipe, DefaultPipe, TruncatePipe],
 })
 export class PipesModule {}
