6e47931c832711a630e6a6d53fe7bab8838825e7
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-04-19 23:11:23 +0200

    used shx instead

--- packages/altair-core/scripts/generate-settings-schema.sh
+++ packages/altair-core/scripts/generate-settings-schema.sh
@@ -2,8 +2,8 @@
 
 yarn typescript-json-schema --ignoreErrors src/types/state/settings.interfaces.ts SettingsState  --out build/settings.schema.json
 yarn ajv compile -s build/settings.schema.json -o build/validate-settings.js
-yarn cpy src/validate-settings.d.ts build/
+yarn shx cp src/validate-settings.d.ts build/validate-settings.d.ts
 
 yarn typescript-json-schema --ignoreErrors src/types/state/settings.interfaces.ts PartialSettingsState  --out build/partial_settings.schema.json
 yarn ajv compile -s build/partial_settings.schema.json -o build/validate-partial-settings.js
-yarn cpy src/validate-partial-settings.d.ts build/
+yarn shx cp src/validate-partial-settings.d.ts build/validate-partial-settings.d.ts
