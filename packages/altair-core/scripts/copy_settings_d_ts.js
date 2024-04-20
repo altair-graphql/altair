d99f1f65906f66cb399b66ab23216cb1a4ca0795
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-04-20 02:30:28 +0200

    moved logic to js script

--- packages/altair-core/scripts/copy_settings_d_ts.js
+++ packages/altair-core/scripts/copy_settings_d_ts.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 const srcDir = path.resolve(__dirname, '../src');
 const distSrc = path.resolve(__dirname, '../build');
+
+execSync(
+  'yarn typescript-json-schema --ignoreErrors src/types/state/settings.interfaces.ts SettingsState  --out build/settings.schema.json'
+);
+execSync(
+  'yarn ajv compile -s build/settings.schema.json -o build/validate-settings.js'
+);
+
+execSync(
+  'yarn typescript-json-schema --ignoreErrors src/types/state/settings.interfaces.ts PartialSettingsState  --out build/partial_settings.schema.json'
+);
+execSync(
+  'yarn ajv compile -s build/partial_settings.schema.json -o build/validate-partial-settings.js'
+);
 
 fs.copyFileSync(
   path.resolve(srcDir, 'validate-settings.d.ts'),
