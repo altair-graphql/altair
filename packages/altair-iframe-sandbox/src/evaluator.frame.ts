1e8b51e82144d108f8c5071323206eddd38bf1b4
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-05-30 00:13:37 +0200

    check against earlier registered origin instead of valid origins

--- packages/altair-iframe-sandbox/src/evaluator.frame.ts
+++ packages/altair-iframe-sandbox/src/evaluator.frame.ts
@@ -2,10 +2,9 @@ import {
   ScriptEvaluatorWorker,
   ScriptWorkerMessageData,
 } from 'altair-graphql-core/build/script/types';
-import { validOrigins } from 'altair-graphql-core/build/origins';
 
 export class EvaluatorFrameWorker extends ScriptEvaluatorWorker {
-  origin = '';
+  private origin: string;
 
   constructor() {
     super();
@@ -13,7 +12,7 @@ export class EvaluatorFrameWorker extends ScriptEvaluatorWorker {
     // Get the source origin that embeds the iframe from the URL query parameter
     const source = params.get('sc');
 
-    if (!source || !validOrigins.includes(source)) {
+    if (!source) {
       throw new Error('Invalid source provided!');
     }
     this.origin = source;
@@ -21,7 +20,7 @@ export class EvaluatorFrameWorker extends ScriptEvaluatorWorker {
 
   onMessage(handler: (e: ScriptWorkerMessageData) => void): void {
     window.addEventListener('message', (e) => {
-      if (e.origin && !validOrigins.includes(e.origin)) {
+      if (e.origin !== this.origin) {
         return;
       }
       handler(e.data);
