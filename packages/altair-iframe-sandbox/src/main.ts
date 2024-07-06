cfb5854b388c2f9aca88b3a5342c7f6d7643c166
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-07-06 20:19:08 +0200

    remove TODO comments

--- packages/altair-iframe-sandbox/src/main.ts
+++ packages/altair-iframe-sandbox/src/main.ts
@@ -4,7 +4,6 @@ import { EvaluatorFrameWorker } from './evaluator.frame';
 import { handlePluginSandbox } from './plugin-sandbox';
 
 const searchParams = new URLSearchParams(window.location.search);
-// TODO: Set sandbox type in parent worker
 const sandboxType = searchParams.get('sandbox_type');
 switch (sandboxType) {
   case 'plugin':
