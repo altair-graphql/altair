0a638f3359cd5e23be6df5561fdd5eb924e17c7c
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-08-31 08:01:51 +0200

    comment out polyfill change

--- packages/altair-app/src/polyfills.ts
+++ packages/altair-app/src/polyfills.ts
@@ -36,7 +36,7 @@
 /** Evergreen browsers require these. **/
 import 'core-js/es/reflect';
 
-(window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['message'];
+// (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['message'];
 
 /** *************************************************************************************************
  * Zone JS is required by Angular itself.
