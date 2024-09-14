abb4a4d9b6029d1c5f8f40c2b93ee98df7e500c0
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-09-14 22:43:02 +0200

    only update URL if there is a consumed value

--- packages/altair-app/src/app/modules/altair/utils/url.ts
+++ packages/altair-app/src/app/modules/altair/utils/url.ts
@@ -1,9 +1,18 @@
+import { debug } from './logger';
+
 export const consumeQueryParam = (key: string) => {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const value = urlSearchParams.get(key);
-  urlSearchParams.delete(key);
-  const params = urlSearchParams.toString();
-  const newUrl = `${window.location.pathname}${params ? '?' : ''}${params}${window.location.hash}`;
-  window.history.replaceState({}, '', newUrl);
-  return value;
+  try {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const value = urlSearchParams.get(key);
+    if (!value) {
+      return;
+    }
+    urlSearchParams.delete(key);
+    const params = urlSearchParams.toString();
+    const newUrl = `${window.location.pathname}${params ? '?' : ''}${params}${window.location.hash}`;
+    window.history.replaceState({}, '', newUrl);
+    return value;
+  } catch (e) {
+    debug.error('Error consuming query param', e);
+  }
 };
