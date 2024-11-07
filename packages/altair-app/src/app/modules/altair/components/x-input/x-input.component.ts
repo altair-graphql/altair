22188f22829488163dfe172e3a3dd32976bbe4a4
Samuel Imolorhe <samuelimolo4real@gmail.com>
2024-11-07 11:15:11 +0100

    used recommended solution

--- packages/altair-app/src/app/modules/altair/components/x-input/x-input.component.ts
+++ packages/altair-app/src/app/modules/altair/components/x-input/x-input.component.ts
@@ -135,25 +135,20 @@ export class XInputComponent implements AfterViewInit, ControlValueAccessor {
 
     const filterNewLine = EditorState.transactionFilter.of((tr) => {
       if (tr.changes.empty) return tr;
-      if (tr.newDoc.lines > 1 && !tr.isUserEvent('input.paste')) {
-        return [];
+
+      if (tr.isUserEvent('input.paste')) {
+        // For paste events, replace newlines with spaces
+        const changes = [
+          {
+            from: 0,
+            insert: tr.newDoc.toString().replace(/\n/g, ' '),
+          },
+        ];
+        return [{ changes }];
       }
 
-      const removeNLs: ChangeSpec[] = [];
-      tr.changes.iterChanges((fromA, toA, fromB, toB, ins) => {
-        const lineIter = ins.iterLines().next();
-        if (ins.lines <= 1) return;
-        // skip the first line
-        let len = fromB + lineIter.value.length;
-        lineIter.next();
-        // for the next lines, remove the leading NL
-        for (; !lineIter.done; lineIter.next()) {
-          removeNLs.push({ from: len, to: len + 1 });
-          len += lineIter.value.length + 1;
-        }
-      });
-
-      return [tr, { changes: removeNLs, sequential: true }];
+      // Block multi-line input from other sources
+      return tr.newDoc.lines > 1 ? [] : [tr];
     });
 
     return [
