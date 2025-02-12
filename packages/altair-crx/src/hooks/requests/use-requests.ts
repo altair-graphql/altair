e44d5ae307e9e43bdd67b98ca2b47458b891446d
Samuel Imolorhe <samuelimolo4real@gmail.com>
2025-02-12 08:35:26 +0100

    clear requests on tab reload

--- packages/altair-crx/src/hooks/requests/use-requests.ts
+++ packages/altair-crx/src/hooks/requests/use-requests.ts
@@ -1,16 +1,67 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { mockRequests } from './mock-requests';
 import { getRequest } from '../../helpers/request';
 import { GraphQLRequest } from '../../types';
+import { isExtension } from '../../helpers/messaging';
 
+const useCurrentTab = () => {
+  const [tabId, setTabId] = useState<number | undefined>(undefined);
+  const browser = window.chrome || window.browser;
+
+  if (!isExtension()) {
+    return;
+  }
+  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length) {
+      const tabId = tabs[0]?.id;
+      if (tabId) {
+        setTabId(tabId);
+      }
+    }
+  });
+
+  return tabId;
+};
+const useTabStatus = () => {
+  const currentTabId = useCurrentTab();
+  const [tabStatus, setTabStatus] = useState<'loading' | 'complete' | 'unloaded'>(
+    'unloaded'
+  );
+  const browser = window.chrome || window.browser;
+
+  if (!isExtension()) {
+    return tabStatus;
+  }
+  browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tabId === currentTabId) {
+      if (changeInfo.status && changeInfo.status !== tabStatus) {
+        setTabStatus(changeInfo.status as 'loading' | 'complete');
+      }
+    }
+  });
+
+  // TODO: Add cleanup logic
+
+  return tabStatus;
+};
 export const useGraphQLRequests = () => {
+  const browser = window.chrome || window.browser;
   const [requests, setRequests] = useState<GraphQLRequest[]>([]);
-  if ('chrome' in window === false || 'devtools' in chrome === false) {
+  const tabStatus = useTabStatus();
+  useEffect(() => {
+    if (tabStatus === 'loading') {
+      // Clear requests when the tab is loading
+      setRequests([]);
+    }
+  }, [tabStatus]);
+
+  if (!isExtension()) {
     return {
       requests: mockRequests,
     };
   }
-  chrome.devtools.network.onRequestFinished.addListener(async (request) => {
+
+  browser.devtools.network.onRequestFinished.addListener(async (request) => {
     const data = await getRequest(request);
     if (!data) {
       return;
