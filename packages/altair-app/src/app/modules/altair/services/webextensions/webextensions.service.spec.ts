fb988deb5e34f80c54f10d66a5dadacbadf698ca
Samuel Imolorhe <samuelimolo4real@gmail.com>
2025-02-12 04:30:13 +0100

    updated test

--- packages/altair-app/src/app/modules/altair/services/webextensions/webextensions.service.spec.ts
+++ packages/altair-app/src/app/modules/altair/services/webextensions/webextensions.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WebExtensionsService } from './webextensions.service';
+import { MockProvider } from 'ng-mocks';
+import { WindowService } from '../window.service';
 
 describe('WebextensionsService', () => {
   let service: WebExtensionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MockProvider(WindowService)],
+    });
     service = TestBed.inject(WebExtensionsService);
   });
 
