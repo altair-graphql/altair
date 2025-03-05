7fbc9220a167236fc95a9b9a7592e5a1ddbf001e
Samuel Imolorhe <samuelimolo4real@gmail.com>
2025-03-06 00:20:10 +0100

    only enable newrelic in prod env

--- packages/altair-api/src/main.ts
+++ packages/altair-api/src/main.ts
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NEW_RELIC_APP_NAME && process.env.NODE_ENV === 'production') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('newrelic');
 }
