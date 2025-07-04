0f9e4259d6486b93e59135ab113450427960b43a
Samuel Imolorhe <samuelimolo4real@gmail.com>
2025-07-04 23:54:26 +0200

    updated test

--- packages/altair-app/src/app/modules/altair/components/authorization/authorization-oauth2/authorization-oauth2.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/authorization/authorization-oauth2/authorization-oauth2.component.spec.ts
@@ -2,9 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthorizationOauth2Component } from './authorization-oauth2.component';
 import { SharedModule } from 'app/modules/altair/modules/shared/shared.module';
-import { MockModule, MockService } from 'ng-mocks';
+import { MockModule, MockProvider, MockService } from 'ng-mocks';
 import { ComponentModule } from '../../components.module';
 import { NotifyService } from 'app/modules/altair/services';
+import { Store } from '@ngrx/store';
 
 describe('AuthorizationOauth2Component', () => {
   let component: AuthorizationOauth2Component;
@@ -19,6 +20,7 @@ describe('AuthorizationOauth2Component', () => {
           provide: NotifyService,
           useValue: MockService(NotifyService),
         },
+        MockProvider(Store),
       ],
     }).compileComponents();
 
