7ed93ddbd55c1475ee311cf931db651e3d97c7b4
Samuel Imolorhe <samuelimolo4real@gmail.com>
2025-10-18 10:44:35 +0200

    Upgraded to v8.4.0

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir '..\bins\altair_8.3.0_x64_win.exe'
+$fileLocation = Join-Path $toolsDir '..\bins\altair_8.4.0_x64_win.exe'
 $fileHash = Get-FileHash $fileLocation
 
 $packageArgs = @{
