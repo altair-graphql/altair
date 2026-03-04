ea8d3b0d1e9de55dcd67b9eab4478cea7f9f5d78
Ashutosh Khemka <ashutoshkhemka05@gmail.com>
2026-03-04 09:28:28 -0500

    test(banner): add tests for BannerComponent

--- packages/altair-app/src/app/modules/altair/components/banner/banner.component.spec.ts
+++ packages/altair-app/src/app/modules/altair/components/banner/banner.component.spec.ts
@@ -1,10 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BannerComponent } from './banner.component';
+import { Banner } from '../../services/banner/banner.service';
 
 describe('BannerComponent', () => {
   let component: BannerComponent;
   let fixture: ComponentFixture<BannerComponent>;
+
+  const mockBanner: Banner = {
+    id: 'test-banner-id',
+    enabled: true,
+    dismissible: true,
+    message: 'Test banner message',
+    icon: 'info',
+    type: 'info',
+    actions: [],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,5 +28,43 @@ describe('BannerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onDismiss', () => {
+    it('should emit the banner id when banner has an id', () => {
+      fixture.componentRef.setInput('banner', mockBanner);
+      fixture.detectChanges();
+
+      const dismissSpy = jest.fn();
+      component.dismiss.subscribe(dismissSpy);
+
+      component.onDismiss();
+
+      expect(dismissSpy).toHaveBeenCalledWith('test-banner-id');
+    });
+
+    it('should not emit if banner is undefined', () => {
+      fixture.componentRef.setInput('banner', undefined);
+      fixture.detectChanges();
+
+      const dismissSpy = jest.fn();
+      component.dismiss.subscribe(dismissSpy);
+
+      component.onDismiss();
+
+      expect(dismissSpy).not.toHaveBeenCalled();
+    });
+
+    it('should not emit if banner has no id', () => {
+      fixture.componentRef.setInput('banner', { ...mockBanner, id: undefined });
+      fixture.detectChanges();
+
+      const dismissSpy = jest.fn();
+      component.dismiss.subscribe(dismissSpy);
+
+      component.onDismiss();
+
+      expect(dismissSpy).not.toHaveBeenCalled();
+    });
   });
 });
