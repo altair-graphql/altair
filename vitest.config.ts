d204d78e7c3a06c6601096d687047244210adf03
Samuel Imolorhe <samuelimolo4real@gmail.com>
2026-03-14 08:57:56 +0100

    added test coverage to workflow

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    projects: [
+      'packages/altair-app',
+      'packages/altair-api',
+      'packages/altair-core',
+      'packages/altair-electron',
+    ],
+    coverage: {
+      provider: 'v8',
+      // reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
+  },
+});
