dbb1dc7a26bb1846d3b8d991b4d7e78d2aa7cd97
Samuel Imolorhe <samuelimolo4real@gmail.com>
2026-03-14 09:08:43 +0100

    added reporters

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     ],
     coverage: {
       provider: 'v8',
-      // reporter: ['text', 'json-summary', 'json'],
+      reporter: ['text', 'json-summary', 'json'],
       reportOnFailure: true,
     },
   },
